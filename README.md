bf9d069f57ab81b52073233e75d6f861ff7f4efb
Edil Usobaiev <usobaiev1@gmail.com>
2023-07-09 12:28:59 +0600

    Add method POST to create image

---
+++ README.md
@@ -0,0 +1,33 @@
+# AI Generated Image Creator
+
+[![License](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+
+## Description
+
+This project allows you to generate AI-generated images using the OpenAI API. It provides a web interface where you can input prompts and generate images based on those prompts.
+
+## Installation
+
+1. Clone the repository:
+
+
+> git clone https://github.com/edilsobai/AI-Generated-Image-Creator.git
+
+2. Install the dependencies:
+> npm install
+
+## Usage
+1. Set up your OpenAI API key by updating the OPENAI_KEY value in the config/default.json file.
+
+2. Start the server:
+> npm start
+
+3. Open your web browser and navigate to http://localhost:5000.
+
+4. Enter a prompt and click on the "Generate Image" button to generate an AI-generated image based on the prompt.
+
+## Contributing
+ If you find any issues or want to contribute enhancements, please open an issue or submit a pull request in the GitHub repository.
+
+## Contact
+For any questions or inquiries, please reach out to sobaiedil@gmail.com.
--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const { engine } = require("express-handlebars")
-const openai = require("openai")
+const { Configuration, OpenAIApi} = require("openai")
 const config = require("config")
 
 const app = express()
-
-const configuration = new openai.Configuration({
+const configuration = new Configuration({
     apiKey: config.get("OPENAI_KEY")
 }) 
+const openai = new OpenAIApi(configuration)
 
 app.engine("handlebars", engine())
 app.set("view engine", "handlebars")
@@ -19,10 +19,26 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
     const { prompt, number, size } = req.body
-    console.log(prompt,number,size)
-    res.render("index")
+
+    try {
+        const response = await openai.createImage({
+            prompt,
+            n: Number(number),
+            size
+        })
+        console.log(response.data.data)
+        res.render("index",{
+            images: response.data.data
+        })
+        
+    } catch (err) {
+        console.log(err)
+        res.render("index", {
+            err: "Error with query"
+        })
+    }
 })
 
 app.listen(5000, () => console.log("Running on PORT: 5000"))
